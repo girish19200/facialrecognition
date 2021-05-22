6b2f2f6e3aa5b3322e51b3bb0fcf196bf7b088d2
girish19200 <84622419+girish19200@users.noreply.github.com>
2021-05-22 18:09:03 +0530

    Add files via upload

---
+++ Nik.py
@@ -0,0 +1,281 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mess
+from PIL import Image,ImageTk
+import tkinter.simpledialog as tsd
+import cv2,os
+import csv
+import numpy as np
+from PIL import Image
+import pandas as pd
+import datetime
+import time
+import random
+from tkinter import BOTH, END, LEFT
+from os import environ
+
+
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+
+def tick():
+    time_string = time.strftime('%H:%M:%S')
+    clock.config(text=time_string)
+    clock.after(200,tick)
+
+
+
+def check_haarcascadefile():
+    exists = os.path.isfile("haarcascade_frontalface_default.xml")
+    if exists:
+        pass
+    else:
+        mess._show(title='Some file missing', message='Please contact us for help')
+        window.destroy()
+
+
+
+def clear():
+    txt.delete(0, 'end')
+    res = "1)Take Images  >>>  2)Save Profile"
+    message1.configure(text=res)
+
+
+def clear2():
+    txt2.delete(0, 'end')
+    res = "1)Take Images  >>>  2)Save Profile"
+    message1.configure(text=res)
+
+
+
+def TakeImages():
+    check_haarcascadefile()
+    columns = ['SERIAL NO.', '', 'ID', '', 'NAME']
+    assure_path_exists("StudentDetails/")
+    assure_path_exists("TrainingImage/")
+    serial = 0
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+    if exists:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for l in reader1:
+                serial = serial + 1
+        serial = (serial // 2)
+        csvFile1.close()
+    else:
+        with open("StudentDetails\StudentDetails.csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(columns)
+            serial = 1
+        csvFile1.close()
+    Id = (txt.get())
+    name = (txt2.get())
+    if ((name.isalpha()) or (' ' in name)):
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+        while (True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                # incrementing sample number
+                sampleNum = sampleNum + 1
+                # saving the captured face in the dataset folder TrainingImage
+                cv2.imwrite("TrainingImage\ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
+                            gray[y:y + h, x:x + w])
+                # display the frame
+                cv2.imshow('Taking Images', img)
+            # wait for 100 miliseconds
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            # break if the sample number is morethan 100
+            elif sampleNum > 100:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        res = "Images Taken for ID : " + Id
+        row = [serial, '', Id, '', name]
+        with open('StudentDetails\StudentDetails.csv', 'a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        message1.configure(text=res)
+    else:
+        if (name.isalpha() == False):
+            res = "Enter Correct name"
+            message.configure(text=res)
+
+
+
+def TrainImages():
+    check_haarcascadefile()
+    assure_path_exists("TrainingImageLabel/")
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    detector = cv2.CascadeClassifier(harcascadePath)
+    faces, ID = getImagesAndLabels("TrainingImage")
+    try:
+        recognizer.train(faces, np.array(ID))
+    except:
+        mess._show(title='No Registrations', message='Please Register someone first!!!')
+        return
+    recognizer.save("TrainingImageLabel\Trainner.yml")
+    res = "Profile Saved Successfully"
+    message1.configure(text=res)
+    message.configure(text='Total Registrations till now  : ' + str(ID[0]))
+
+
+
+def getImagesAndLabels(path):
+    # get the path of all the files in the folder
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    # create empth face list
+    faces = []
+    # create empty ID list
+    Ids = []
+    # now looping through all the image paths and loading the Ids and the images
+    for imagePath in imagePaths:
+        # loading the image and converting it to gray scale
+        pilImage = Image.open(imagePath).convert('L')
+        # Now we are converting the PIL image into numpy array
+        imageNp = np.array(pilImage, 'uint8')
+        # getting the Id from the image
+        ID = int(os.path.split(imagePath)[-1].split(".")[1])
+        # extract the face from the training image sample
+        faces.append(imageNp)
+        Ids.append(ID)
+    return faces, Ids
+
+
+
+
+
+    
+global key
+key = ''
+
+ts = time.time()
+date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+day,month,year=date.split("-")
+
+mont={'01':'January',
+      '02':'February',
+      '03':'March',
+      '04':'April',
+      '05':'May',
+      '06':'June',
+      '07':'July',
+      '08':'August',
+      '09':'September',
+      '10':'October',
+      '11':'November',
+      '12':'December'
+      }
+
+
+
+window = tk.Tk()
+window.geometry("1280x720")
+window.resizable(True,False)
+window.title("BIOMETRICS SYSTEM")
+window.configure(background='#a8c0ff')
+
+
+def entryrandom():
+    entry_random = str(random.randint(1111111111,9999999999))
+    txt.insert(END, entry_random)
+
+# Create a photoimage object of the image in the path
+image1 = Image.open("images.jpg")
+test = ImageTk.PhotoImage(image1)
+
+label1 = tk.Label(image=test)
+label1.image = test
+
+# Position image
+label1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
+
+
+#frame1 = ImageTk.PhotoImage(file = "Bit.jpg")
+
+#frame1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
+
+frame2 = tk.Frame(window, bg="#8f94fb")
+frame2.place(relx=0.51, rely=0.17, relwidth=0.38, relheight=0.80)
+
+message3 = tk.Label(window, text="E-VOTING SYSTEM (BIOMETRICS)" ,fg="#3f2b96",bg="#a8c0ff" ,width=55 ,height=1,font=('Ubuntu', 29, ' bold '))
+message3.place(x=10, y=10)
+
+frame3 = tk.Frame(window, bg="#c4c6ce")
+frame3.place(relx=0.52, rely=0.09, relwidth=0.09, relheight=0.07)
+
+frame4 = tk.Frame(window, bg="#c4c6ce")
+frame4.place(relx=0.36, rely=0.09, relwidth=0.16, relheight=0.07)
+
+datef = tk.Label(frame4, text = day+"-"+mont[month]+"-"+year+"  |  ", fg="#3f2b96",bg="#a8c0ff" ,width=55 ,height=1,font=('Ubuntu', 22, ' bold '))
+datef.pack(fill='both',expand=1)
+
+clock = tk.Label(frame3,fg="#3f2b96",bg="#a8c0ff" ,width=55 ,height=1,font=('Ubuntu', 22, ' bold '))
+clock.pack(fill='both',expand=1)
+tick()
+
+
+lbl = tk.Label(frame2, text="  Enter ID",width=10  ,height=1  ,fg="#3f2b96"  ,bg="#8f94fb" ,font=('Ubuntu', 17, ' bold ') )
+lbl.place(x=0.005, y=88)
+
+txt = tk.Entry(frame2,width=15  ,fg="#3f2b96",font=('Ubuntu', 15, ' bold '))
+txt.place(x=165, y=88)
+
+lbl2 = tk.Label(frame2, text="Enter Name",width=20  ,fg="#3f2b96"  ,bg="#8f94fb" ,font=('Ubuntu', 17, ' bold '))
+lbl2.place(x=-45, y=173)
+
+txt2 = tk.Entry(frame2,width=15 ,fg="#3f2b96",font=('Ubuntu', 15, ' bold ')  )
+txt2.place(x=165, y=173)
+
+message1 = tk.Label(frame2, text="1)Take Images  >>>  2)Save Profile" ,bg="#8f94fb" ,fg="#3f2b96"  ,width=39 ,height=1, activebackground = "yellow" ,font=('Ubuntu', 15, ' bold '))
+message1.place(x=7, y=230)
+
+message = tk.Label(frame2, text="" ,bg="#8f94fb" ,fg="#3f2b96"  ,width=39,height=1, activebackground = "yellow" ,font=('Ubuntu', 16, ' bold '))
+message.place(x=7, y=450)
+
+
+res=0
+exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+if exists:
+    with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+        reader1 = csv.reader(csvFile1)
+        for l in reader1:
+            res = res + 1
+    res = (res // 2) - 1
+    csvFile1.close()
+else:
+    res = 0
+message.configure(text='Total Registrations   : '+str(res))
+
+
+
+
+clearButton = tk.Button(frame2, text = "Generate",command = entryrandom ,fg="white"  ,bg="#3f2b96"  ,width=11 ,activebackground = "white" ,font=('times', 11, ' bold '))
+clearButton.place(x=335, y=86)
+clearButton2 = tk.Button(frame2, text="Clear", command=clear2  ,fg="white"  ,bg="#3f2b96"  ,width=11 , activebackground = "white" ,font=('times', 11, ' bold '))
+clearButton2.place(x=335, y=172)    
+takeImg = tk.Button(frame2, text="Take Images", command=TakeImages  ,fg="white"  ,bg="#3f2b96"  ,width=34  ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+takeImg.place(x=30, y=300)
+trainImg = tk.Button(frame2, text="Save Profile", command=TrainImages ,fg="white"  ,bg="#3f2b96"  ,width=34  ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+trainImg.place(x=30, y=380)
+
+
+
+
+
+window.mainloop()
+
+
